514f774faccd97fd9e3cdfafee0013e5d38415de
unknown <user@mil8a-315-11.mmcs.sfedu.ru>
2019-03-13 12:42:47 +0300

    Add ReplaceExpr

--- SimpleLanguage2/Visitors/ChangeVisitor.cs
+++ SimpleLanguage2/Visitors/ChangeVisitor.cs
@@ -9,6 +9,39 @@ namespace SimpleLang.Visitors
 {
 	class ChangeVisitor : AutoVisitor
 	{
+		public void ReplaceExpr(ExprNode from, ExprNode to)
+		{
+			var p = from.Parent;
+			if (to != null)
+				to.Parent = p;
+			if (p is AssignNode assn)
+			{
+				assn.Expr = to;
+			}
+			else if (p is BinOpNode binopn)
+			{
+				if (binopn.Left == from) // Поиск подузла в Parent
+					binopn.Left = to;
+				else if (binopn.Right == from)
+					binopn.Right = to;
+			}
+			else if (p is ForNode forn)
+			{
+				if (forn.Start == from) // Поиск подузла в Parent
+					forn.Start = to;
+				else if (forn.End == from)
+					forn.End = to;
+			}
+			else if (p is PrintlnNode pn)
+			{
+				if (pn.Expr == from) // Поиск подузла в Parent
+					pn.Expr = to;
+			}
+			else if (p is BlockNode)
+			{
+				throw new Exception("Родительский узел не содержит выражений");
+			}
+		}
 		public void ReplaceStat(StatementNode from, StatementNode to)
 		{
 			var p = from.Parent;
