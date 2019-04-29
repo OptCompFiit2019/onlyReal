1e5b6cd70f744864f55b0250a8bd1daa7c1d7675
mestydone <mestydone@gmail.com>
2019-04-29 18:54:34 +0300

    create transmission function class

---
+++ OptimCompiler/TransmissionFunction/TransmissionFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang.TransmissionFunction
+{
+
+    struct InOut
+    {
+        public ISet<string> In;
+        public ISet<string> Out;
+
+        public InOut(ISet<string> inSet, ISet<string> outSet)
+        {
+            In = inSet;
+            Out = outSet;
+        }
+    }
+
+    class TransmissionFunction
+    {
+        private Func<InOut, InOut> algorithm;
+
+        public TransmissionFunction(Func<InOut, InOut> algorithm)
+        {
+            this.algorithm = algorithm;
+        } 
+
+        public InOut Apply(InOut inOut) => algorithm.Invoke(inOut);        
+    }
+}
