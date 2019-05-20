886c3a88168011eff79251be854c70bd712f22c0
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2019-05-20 19:30:27 +0300

    Generic ItA updated

--- OptimCompiler/GenericIterativeAlgorithm/IterativeAlgorithm.cs
+++ OptimCompiler/GenericIterativeAlgorithm/IterativeAlgorithm.cs
@@ -4,49 +4,105 @@ using TransferFunctionGeneric;
 
 namespace SimpleLang.GenericIterativeAlgorithm
 {
-    class test
+    /// <summary>
+    /// Вспомогательный класс, хранящий информацию о множествах отдельного блока, таких как
+    /// IN, OUT, def, use, gen, kill, e_gen, e_kill
+    /// </summary>
+    class BlockInfo
     {
-        IterativeAlgorithm it = new IterativeAlgorithm(true, null, string, string);
+        public ISet<string> IN;
+        public ISet<string> OUT;
+        public ISet<string> HelpFirst; // Аналог def_b, gen_b, e_gen_b
+        public ISet<string> HelpSecond; // Аналог use_b, kill_b, e_kill_b
+
+        /// <summary>
+        /// Определяет равенство текущего объекта и <paramref name="other"/> по совпадению полей.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(BlockInfo other)
+        {
+            if (IN.Count != other.IN.Count || OUT.Count != other.OUT.Count
+                || HelpFirst.Count != other.HelpFirst.Count || HelpSecond.Count != other.HelpSecond.Count)
+                return false;
+
+            return IN.SetEquals(other.IN) && OUT.SetEquals(other.OUT) 
+                && HelpFirst.SetEquals(other.HelpFirst) && HelpSecond.SetEquals(other.HelpSecond);
+        }
+
+        public BlockInfo(BlockInfo source)
+        {
+            IN = new HashSet<string>(source.IN);
+            OUT = new HashSet<string>(source.OUT);
+            HelpFirst = new HashSet<string>(source.HelpFirst);
+            HelpSecond = new HashSet<string>(source.HelpSecond);
+        }
+
+        public BlockInfo(ISet<string> helpFirst, ISet<string> helpSecond)
+        {
+            IN = new HashSet<string>();
+            OUT = new HashSet<string>();
+            HelpFirst = helpFirst;
+            HelpSecond = helpSecond;
+        }
+
+        public BlockInfo(ISet<string> helpFirst, ISet<string> helpSecond, ISet<string> In, ISet<string> Out)
+        {
+            IN = In;
+            OUT = Out;
+            HelpFirst = helpFirst;
+            HelpSecond = helpSecond;
+        }
     }
 
     class IterativeAlgorithm
     {
+        // направление обхода
         public bool IsForward { get;}
         public ControlFlowGraph.ControlFlowGraph Graph { get; }
-        public List<IBlockInfo> IN { get; }
-        public List<IBlockInfo> OUT { get; }
-        public Func<ControlFlowGraph.ControlFlowGraph, int, IBlockInfo> MeetOperator;
-        public TransferFunction<IBlockInfo> Function { get; }
-        public IBlockInfo InitEntryExit { get; }
-        public IBlockInfo InitOther { get; }
+        // информация о блоках. Каждому блоку с индексом i в графе соответствует экземпляр
+        // класса BlockInfo в списке BlocksInfo с тем же индексом
+        public List<BlockInfo> BlocksInfo { get; }
+        // оператор сбора
+        public Func<List<BlockInfo>, ControlFlowGraph.ControlFlowGraph, int, BlockInfo> MeetOperator;
+        // передаточная функция
+        public TransferFunction<BlockInfo> Function { get; }
+        // значение инициализации IN/OUT первого или последнего блока для прямого или обратного обходов соответственно
+        public ISet<string> InitEntryExit { get; }
+        // значение инициализации IN/OUT для остальных блоков
+        public ISet<string> InitOther { get; }
 
-        public IterativeAlgorithm(bool isForward, ControlFlowGraph.ControlFlowGraph graph, IBlockInfo initValueEntryExit, IBlockInfo initValueOthers)
+        public IterativeAlgorithm(List<BlockInfo> blocksInfo, ControlFlowGraph.ControlFlowGraph graph, 
+            Func<List<BlockInfo>, ControlFlowGraph.ControlFlowGraph, int, BlockInfo> meetOperator, bool isForward, 
+            IEnumerable<string> initValueEntryExit, IEnumerable<string> initValueOthers,
+            TransferFunction<BlockInfo> function)
         {
-            IsForward = isForward;
+            BlocksInfo = blocksInfo;
             Graph = graph;
-            IN = new List<IBlockInfo>();
-            OUT = new List<IBlockInfo>();
+            MeetOperator = meetOperator;
+            IsForward = isForward;
+            InitEntryExit = new HashSet<string>(initValueEntryExit);
+            InitOther = new HashSet<string>(InitOther);
         }
 
-        public void Perform()
+        /// <summary>
+        /// Выполняет оптимизационный алгоритм и возвращает результат как List<BlockInfo>,
+        /// </summary>
+        /// <returns></returns>
+        public List<BlockInfo> Perform()
         {
             int countBlocks = Graph.blocks.Count;
-            // инициализация OUT или IN входа или выхода заданным значением
-            // for(каждый ББл, отличный от того, который выше) инициализация Остальных OUT или IN
+
             for (int i = 0; i < countBlocks; i++)
             {
-                IN.Add(InitOther);
-                OUT.Add(InitOther);
-            }
-
-            List<IBlockInfo> MeetResult, FunctionResult;
-            MeetResult = IsForward ? IN : OUT;
-            FunctionResult = IsForward ? OUT : IN;        
+                BlocksInfo[i].IN = InitEntryExit;
+                BlocksInfo[i].OUT = InitOther;
+            }   
 
             if (IsForward)
-                FunctionResult[0] = InitEntryExit;
+                BlocksInfo[0].OUT = InitEntryExit;
             else
-                FunctionResult[countBlocks - 1] = InitEntryExit;
+                BlocksInfo[countBlocks - 1].IN = InitEntryExit;
 
             int startIndex = IsForward ? 1 : 0;
             int endIndex = IsForward ? countBlocks : countBlocks - 1;
@@ -57,19 +113,14 @@ namespace SimpleLang.GenericIterativeAlgorithm
                 isChanged = false;
                 for(int index = startIndex; index < endIndex; index++)
                 {
-                    var prevFunctionResult = (IBlockInfo)FunctionResult[index].Clone();
-                    MeetResult[index] = MeetOperator(Graph, index);
-                    FunctionResult[index] = Function.Apply(MeetResult[index]);
-                    isChanged = isChanged || prevFunctionResult.Changed(FunctionResult[index]);
+                    var prevFunctionResult = new BlockInfo(BlocksInfo[index]);
+                    BlocksInfo[index] = MeetOperator(BlocksInfo, Graph, index);
+                    BlocksInfo[index] = Function.Apply(BlocksInfo[index]);
+                    isChanged = isChanged || prevFunctionResult.Equals(BlocksInfo[index]);
                 }
             }
+            return BlocksInfo;
         }
-    }
-
-
-    public interface IBlockInfo : ICloneable
-    {
-        bool Changed(IBlockInfo previousState);
     }
 
 }
