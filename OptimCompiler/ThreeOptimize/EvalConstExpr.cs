2755745fad794ce18aef9e905f17a21364c221f5
Dmitrj PRO <sr8212@myprogram.us>
2019-05-26 15:23:37 +0200

    Fix bug int optimise const expr

--- OptimCompiler/ThreeOptimize/EvalConstExpr.cs
+++ OptimCompiler/ThreeOptimize/EvalConstExpr.cs
@@ -16,6 +16,11 @@ namespace SimpleLang.ThreeCodeOptimisations{
             it.Value.arg1 = new Visitors.ThreeAddressIntValue(res);
             _apply = true;
         }
+        private void Set(LinkedListNode<Visitors.ThreeCode> it, double res) {
+            it.Value.operation = Visitors.ThreeOperator.Assign;
+            it.Value.arg1 = new Visitors.ThreeAddressDoubleValue(res);
+            _apply = true;
+        }
         private void Set(LinkedListNode<Visitors.ThreeCode> it, bool res){
             it.Value.operation = Visitors.ThreeOperator.Assign;
             it.Value.arg1 = new Visitors.ThreeAddressLogicValue(res);
@@ -35,6 +40,11 @@ namespace SimpleLang.ThreeCodeOptimisations{
                 bool barg2 = false;
                 bool barg2val = false;
 
+                double darg1 = 0;
+                bool darg1val = false;
+                double darg2 = 0;
+                bool darg2val = false;
+
                 if (it.Value.arg1 != null) {
                     if (it.Value.arg1 is Visitors.ThreeAddressIntValue val) {
                         iarg1val = true;
@@ -43,6 +53,10 @@ namespace SimpleLang.ThreeCodeOptimisations{
                     if (it.Value.arg1 is Visitors.ThreeAddressLogicValue vall) {
                         iarg1val = true;
                         barg1 = vall.Value;
+                    }
+                    if (it.Value.arg1 is Visitors.ThreeAddressDoubleValue val2) {
+                        darg1val = true;
+                        darg1 = val2.Value;
                     }
                 }
 
@@ -54,6 +68,10 @@ namespace SimpleLang.ThreeCodeOptimisations{
                     if (it.Value.arg2 is Visitors.ThreeAddressLogicValue vall) {
                         iarg2val = true;
                         barg2 = vall.Value;
+                    }
+                    if (it.Value.arg2 is Visitors.ThreeAddressDoubleValue val2) {
+                        darg2val = true;
+                        darg2 = val2.Value;
                     }
                 }
 
@@ -67,7 +85,7 @@ namespace SimpleLang.ThreeCodeOptimisations{
                     Set(it, barg1 && barg2);
                 }
 
-
+                // For int
                 if (it.Value.operation == Visitors.ThreeOperator.Logic_less && iarg1val && iarg2val){
                     Set(it, iarg1 < iarg2);
                 }
@@ -93,8 +111,60 @@ namespace SimpleLang.ThreeCodeOptimisations{
                     Set(it, !barg1);
                 }
 
-                // Ariphetic
 
+                //Double logic
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_less && darg1val && darg2val){
+                    Set(it, darg1 < darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_equal && darg1val && darg2val) {
+                    Set(it, Math.Abs(darg1 - darg2) < 0.000001);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_greater && darg1val && darg2val){
+                    Set(it, darg1 > darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_geq && darg1val && darg2val){
+                    Set(it, darg1 >= darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_leq && darg1val && darg2val) {
+                    Set(it, darg1 <= darg2);
+                }
+
+                // For int double
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_less && iarg1val && darg2val) {
+                    Set(it, iarg1 < darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_equal && iarg1val && darg2val){
+                    Set(it, Math.Abs(iarg1 - darg2) < 0.000001);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_greater && iarg1val && darg2val){
+                    Set(it, iarg1 > darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_geq && iarg1val && darg2val) {
+                    Set(it, iarg1 >= darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_leq && iarg1val && darg2val){
+                    Set(it, iarg1 <= darg2);
+                }
+
+                // For double int
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_less && darg1val && iarg2val){
+                    Set(it, darg1 < iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_equal && darg1val && iarg2val){
+                    Set(it, Math.Abs(darg1 - iarg2) < 0.000001);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_greater && darg1val && iarg2val){
+                    Set(it, darg1 > iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_geq && darg1val && iarg2val) {
+                    Set(it, darg1 >= iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Logic_leq && darg1val && iarg2val) {
+                    Set(it, darg1 <= iarg2);
+                }
+
+                // Ariphetic
+                // int
                 if (it.Value.operation == Visitors.ThreeOperator.Minus && iarg1val && iarg2val){
                     Set(it, iarg1 - iarg2);
                 }
@@ -106,6 +176,47 @@ namespace SimpleLang.ThreeCodeOptimisations{
                 }
                 if (it.Value.operation == Visitors.ThreeOperator.Div && iarg1val && iarg2val) {
                     Set(it, iarg1 / iarg2);
+                }
+
+                // double
+                if (it.Value.operation == Visitors.ThreeOperator.Minus && darg1val && darg2val) {
+                    Set(it, darg1 - darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Plus && darg1val && darg2val) {
+                    Set(it, darg1 + darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Mult && darg1val && darg2val) {
+                    Set(it, darg1 * darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Div && darg1val && darg2val) {
+                    Set(it, darg1 / darg2);
+                }
+                // int double
+                if (it.Value.operation == Visitors.ThreeOperator.Minus && iarg1val && darg2val) {
+                    Set(it, iarg1 - darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Plus && iarg1val && darg2val) {
+                    Set(it, iarg1 + darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Mult && iarg1val && darg2val) {
+                    Set(it, iarg1 * darg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Div && iarg1val && darg2val) {
+                    Set(it, iarg1 / darg2);
+                }
+
+                // double int
+                if (it.Value.operation == Visitors.ThreeOperator.Minus && darg1val && iarg2val){
+                    Set(it, darg1 - iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Plus && darg1val && iarg2val) {
+                    Set(it, darg1 + iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Mult && darg1val && iarg2val) {
+                    Set(it, darg1 * iarg2);
+                }
+                if (it.Value.operation == Visitors.ThreeOperator.Div && darg1val && iarg2val) {
+                    Set(it, darg1 / iarg2);
                 }
             }
         }
