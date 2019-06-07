9cffa3471878a503e56eb259c540b7551a16dee1
dagribo <dagribo@yandex.ru>
2019-06-07 11:53:10 +0300

    Update PlusNonZero.cs

--- OptimCompiler/AstOptimisations/PlusNonZero.cs
+++ OptimCompiler/AstOptimisations/PlusNonZero.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +6,29 @@ using ProgramTree;
 
 namespace SimpleLang.Visitors
 {
-    class PlusNonZero : ChangeVisitor
+    class PlusNonZero : AutoApplyVisitorInterface
     {
         public override void VisitBinOpNode(BinOpNode binop)
         {
             if (binop.Left is IntNumNode && (binop.Left as IntNumNode).Num == 0 &&
             binop.Op[0] == '+')
             {
-                binop.Right.Visit(this); 
-                ReplaceExpr(binop, binop.Right); 
+                binop.Right.Visit(this);
+                ReplaceExpr(binop, binop.Right);
             }
             else
             {
-                base.VisitBinOpNode(binop); 
+                if (binop.Right is IntNumNode && (binop.Right as IntNumNode).Num == 0 &&
+            binop.Op[0] == '+')
+                {
+                    binop.Left.Visit(this);
+                    ReplaceExpr(binop, binop.Left);
+                }
+                else
+                {
+                    base.VisitBinOpNode(binop);
+                }
+
             }
         }
     }
