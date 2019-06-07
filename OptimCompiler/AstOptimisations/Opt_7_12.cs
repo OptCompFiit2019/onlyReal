aa3e5720ef609fe53d00e04ae41cbfe0b37e242a
IvanMarkov <vanymarkov@rambler.ru>
2019-06-07 16:24:25 +0300

    AST Optimizations, tests, docs

---
+++ OptimCompiler/AstOptimisations/Opt_7_12.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    class Opt7Visitor : AutoApplyVisitorInterface {
+        public override void VisitLogicOpNode (LogicOpNode lop) 
+        {
+            if (lop.Left is ExprNode)
+                lop.Left.Visit(this); 
+
+            if (lop.Right is ExprNode)
+                lop.Right.Visit(this);
+
+            if (lop.Left is IntNumNode l && lop.Right is IntNumNode r && (lop.Operation == "==")) {
+                bool nbool = l.Num == r.Num;
+                if (lop.Parent is IfNode ifn) {
+                    ifn.Cond = new BooleanNode(nbool);
+                    SetApply();
+                } else
+                    if (lop.Parent is WhileNode w) {
+                    w.Expr = new BooleanNode(nbool);
+                    SetApply();
+                    } else
+                        ReplaceExpr(lop, new BooleanNode(nbool));
+            } else {
+                base.VisitLogicOpNode(lop);
+            }
+        }
+    }
+
+    class Opt12Visitor : AutoApplyVisitorInterface {
+        public override void VisitIfNode(IfNode ifn) {
+            if (ifn.Cond is BooleanNode bn && bn.Val == false) {
+                if (ifn.Else != null)
+                    ifn.Else.Visit(this);
+                SetApply();
+                ReplaceStat(ifn, ifn.Else);
+            } else
+                base.VisitIfNode(ifn);
+        }
+    }
+}
