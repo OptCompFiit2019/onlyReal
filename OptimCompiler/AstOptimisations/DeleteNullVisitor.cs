46fad6ec9281595f90638f86acd7b6a570e3e118
Fantom <Vadik426_426@mail.ru>
2019-06-07 03:42:13 +0300

    Add AssignVisitor and DeleteNullVisitor

---
+++ OptimCompiler/AstOptimisations/DeleteNullVisitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    class DeleteNullVisitor: ChangeVisitor
+    {
+        public override void VisitBlockNode(BlockNode bln)
+        {
+            bln.StList = bln.StList.Where(x => x != null).ToList();
+        }
+        public override void VisitIfNode(IfNode ifn)
+        {
+            if (ifn.Else == null && ifn.If == null)
+            {
+                ReplaceStat(ifn, null);
+            }
+        }
+    }
+}
