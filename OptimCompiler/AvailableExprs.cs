556cb5161f77e7ea988ce0a1ff643a74eef564db
Anna Bolotina <ann-bolotina@yandex.ru>
2019-06-09 16:36:53 +0300

    Available expressions optimization + tests

--- OptimCompiler/AvailableExprs.cs
+++ OptimCompiler/AvailableExprs.cs
@@ -4,18 +4,37 @@ using System.Linq;
 using ProgramTree;
 using System.Text;
 using SimpleLang.Block;
+using SimpleLang.GenericIterativeAlgorithm;
+using GenericTransferFunction;
 
 namespace SimpleLang.Visitors
 {
     // arg1 op arg2
-    using ExprSet = HashSet<(String, String, String)>;
-    using KillerSet = HashSet<String>;
-    
+    using Expr = ValueTuple<ThreeAddressValueType, ThreeOperator, ThreeAddressValueType>;
+    using ExprSet = HashSet<(ThreeAddressValueType, ThreeOperator, ThreeAddressValueType)>;
+    using KillerSet = HashSet<string>;
+
+    // Для обратной совместимости с тестирующей системой
+    using StringExprSet = HashSet<(string, string, string)>;
+
+    public class AvaliableExprsAdaptor
+    {
+        public static TransferFunction<BlockInfo<Expr>> TransferFunction()
+            => new TransferFunction<BlockInfo<Expr>>(bi =>
+            {
+                var Out = new BlockInfo<Expr>(bi);
+                Out.OUT = AvaliableExprs.TransferByGenAndKiller(new ExprSet(bi.IN),
+                    AvaliableExprs.GetGenExprSet(bi.Commands), AvaliableExprs.GetKillerSet(bi.Commands));
+                return Out;
+            });
+    }
+
     public class AvaliableExprs
     {
         public static bool IsDefinition(ThreeOperator opType)
         {
-            return opType != ThreeOperator.Logic_not && opType != ThreeOperator.Goto && opType != ThreeOperator.IfGoto;
+            return opType != ThreeOperator.Println && opType != ThreeOperator.Goto
+                && opType != ThreeOperator.IfGoto && opType != ThreeOperator.None;
         }
         public static ExprSet GetGenExprSet(LinkedList<ThreeCode> bblock)
         {
@@ -23,37 +42,49 @@ namespace SimpleLang.Visitors
             var ret = new ExprSet();
             foreach (var line in bblock)
             {
-                if (line.operation == ThreeOperator.Goto || line.operation == ThreeOperator.Println) continue;
+                if (line.operation == ThreeOperator.Goto || line.operation == ThreeOperator.IfGoto
+                        || line.operation == ThreeOperator.Println
+                        || line.operation == ThreeOperator.None) continue;
 
-                ret.RemoveWhere(x => x.Item1 == line.result || x.Item3 == line.result);
+                ret.RemoveWhere(x => x.Item1.ToString() == line.result
+                    || x.Item3.ToString() == line.result);
 
                 if (line.operation == ThreeOperator.Plus || line.operation == ThreeOperator.Mult || line.operation == ThreeOperator.Minus || line.operation == ThreeOperator.Logic_or || line.operation == ThreeOperator.Logic_not || line.operation == ThreeOperator.Logic_neq || line.operation == ThreeOperator.Logic_less || line.operation == ThreeOperator.Logic_leq || line.operation == ThreeOperator.Logic_greater || line.operation == ThreeOperator.Logic_geq || line.operation == ThreeOperator.Logic_equal || line.operation == ThreeOperator.Logic_and || line.operation == ThreeOperator.Div)
                 {
-                    ret.Add((line.arg1.ToString(), line.operation.ToString(), line.arg2.ToString()));
+                    ret.Add((line.arg1, line.operation, line.arg2));
                 }
             }
             return ret;
         }
 
-        public KillerSet GetKillerSet(LinkedList<ThreeCode> bblock)
+        public static KillerSet GetKillerSet(LinkedList<ThreeCode> bblock)
         {
             return new KillerSet(bblock
                                  .Where(l => IsDefinition(l.operation))
                                  .Select(l => l.result));
         }
 
-        public (List<ExprSet>, List<KillerSet>) GetGenAndKillerSets(List<LinkedList<ThreeCode>> bblocks)
+        public (List<StringExprSet>, List<KillerSet>) GetGenAndKillerSets(List<LinkedList<ThreeCode>> bblocks)
         {
-            return (bblocks.Select(b => GetGenExprSet(b)).ToList(),
+            return (bblocks.Select(b => new StringExprSet(GetGenExprSet(b)
+                        .Select(e => (e.Item1.ToString(), e.Item2.ToString(), e.Item3.ToString()))))
+                        .ToList(),
                     bblocks.Select(b => GetKillerSet(b)).ToList());
-
         }
 
-        public ExprSet TransferByGenAndKiller(ExprSet X, ExprSet gen, KillerSet kill)
+        public static ExprSet TransferByGenAndKiller(ExprSet X, ExprSet gen, KillerSet kill)
         {
             if (X == null) return gen;
-            return new ExprSet(X.Where(e => !kill.Contains(e.Item1) && !kill.Contains(e.Item3))
-                               .Union(gen));
+            return new ExprSet(X.Where(e => !kill.Contains(e.Item1.ToString())
+                && !kill.Contains(e.Item3?.ToString())).Union(gen));
+        }
+
+        // Для обратной совместимости с тестирующей системой
+        public StringExprSet TransferByGenAndKiller(StringExprSet X, StringExprSet gen, KillerSet kill)
+        {
+            if (X == null) return gen;
+            return new StringExprSet(X.Where(e => !kill.Contains(e.Item1.ToString())
+                && !kill.Contains(e.Item3.ToString())).Union(gen));
         }
     }
 }
---
+++ OptimCompiler/ThreeCodeOptimisations/AvailableExprsOptimization.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CFG = SimpleLang.ControlFlowGraph.ControlFlowGraph;
+using SimpleLang.Visitors;
+using SimpleLang.GenericIterativeAlgorithm;
+
+namespace SimpleLang.ExprOptimisations
+{
+    using Expr = ValueTuple<ThreeAddressValueType, ThreeOperator, ThreeAddressValueType>;
+
+    class AvailableExprsOptimizer
+    {
+        private CFG controlFlowGraph;
+
+        private int currentTempVarIndex = 0;
+
+        public List<HashSet<Expr>> Ins { get; private set; }
+        public List<HashSet<Expr>> Outs { get; private set; }
+
+        public void IterativeAlgorithm(List<LinkedList<ThreeCode>> blocks)
+        {
+            var bb = new LinkedList<ThreeCode>();
+            bb.AddLast(new ThreeCode("entry", "", ThreeOperator.None, null, null));
+            var bs = blocks.ToList();
+            // добавление пустого входного блока - необходимо для корректной работы ит. алгоритма
+            bs.Insert(0, bb);
+            // построение CFG по блокам
+            controlFlowGraph = new CFG(bs);
+            // создание информации о блоках
+            var blocksInfo = new List<BlockInfo<Expr>>();
+            for (int i = 0; i < bs.Count; i++)
+                blocksInfo.Add(new BlockInfo<Expr>(bs[i]));
+
+            // оператор сбора для доступных выражений
+            Func<List<BlockInfo<Expr>>, CFG, int, BlockInfo<Expr>> meetOperator =
+                (blocksInfos, graph, index) =>
+                {
+                    var inputIndexes = graph.cfg.GetInputNodes(index);
+                    var resInfo = new BlockInfo<Expr>(blocksInfos[index]);
+                    resInfo.IN = resInfo.OUT; // универсальное множество
+                    foreach (var i in inputIndexes)
+                        resInfo.IN.IntersectWith(blocksInfos[i].OUT);
+                    return resInfo;
+                };
+
+            var transferFunction = AvaliableExprsAdaptor.TransferFunction();
+
+            var U = new HashSet<Expr>(blocks.Select(b => b.Where(c =>
+                    AvaliableExprs.IsDefinition(c.operation))
+                .Select(c => (c.arg1, c.operation, c.arg2)))
+                .Aggregate((s1, s2) => s1.Union(s2)));
+
+            // создание объекта итерационного алгоритма
+            var iterativeAlgorithm = new IterativeAlgorithm<Expr>(blocksInfo,
+                controlFlowGraph, meetOperator, true, new HashSet<Expr>(), U, transferFunction);
+
+            // выполнение алгоритма
+            iterativeAlgorithm.Perform();
+            Ins = iterativeAlgorithm.GetINs();
+            Outs = iterativeAlgorithm.GetOUTs();
+        }
+
+        public CFG ApplyOptimization(List<LinkedList<ThreeCode>> blocks)
+        {
+            IterativeAlgorithm(blocks);
+            var bs = controlFlowGraph.blocks;
+
+            for (int i = 1; i < bs.Count; ++i)
+            {
+                for (var it = bs[i].First; true; it = it.Next)
+                {
+                    var command = it.Value;
+                    var expr = (command.arg1, command.operation, command.arg2);
+                    if (Ins[i].Contains(expr))
+                    {
+                        string t = GenTempVariable();
+                        it.Value = new ThreeCode(command.result, new ThreeAddressStringValue(t));
+                        ApplyOptToAncestors(i, expr, t);
+                        if (Outs[i].Contains(expr))
+                            ApplyOptToDescendents(i, expr, t);
+                    }
+
+                    Ins[i].ExceptWith(Ins[i].Where(e => e.Item1.ToString() == command.result
+                        || e.Item3?.ToString() == command.result).ToList());
+                    if (it == bs[i].Last)
+                        break;
+                }
+            }
+            return controlFlowGraph;
+        }
+
+        private void ApplyOptToAncestors(int index, Expr expr, string tempVar)
+        {
+            var bs = controlFlowGraph.blocks;
+
+            var inputIndexes = controlFlowGraph.cfg.GetInputNodes(index);
+            foreach (var ii in inputIndexes)
+            {
+                if (!Outs[ii].Contains(expr))
+                    continue;
+                var ancestor_it = bs[ii].Last;
+                bool applied = false;
+                bool notFirst;
+                while (notFirst = ancestor_it.Value.result != expr.Item1.ToString()
+                        && ancestor_it.Value.result != expr.Item3.ToString())
+                {
+                    if (!ExprEq((ancestor_it.Value.arg1,
+                                ancestor_it.Value.operation,
+                                ancestor_it.Value.arg2), expr))
+                        if (ancestor_it == bs[ii].First)
+                        {
+                            ApplyOptToAncestors(ii, expr, tempVar);
+                            break;
+                        }
+                        else
+                            ancestor_it = ancestor_it.Previous;
+                    else
+                    {
+                        ancestor_it.Value = new ThreeCode(ancestor_it.Value.result,
+                            new ThreeAddressStringValue(tempVar));
+                        applied = true;
+                        Outs[ii].Remove(expr);
+                    }
+                }
+                if (applied)
+                    if (notFirst)
+                        bs[ii].AddBefore(ancestor_it, new ThreeCode(tempVar, expr.Item2,
+                                    expr.Item1, expr.Item3));
+                    else
+                        bs[ii].AddAfter(ancestor_it, new ThreeCode(tempVar, expr.Item2,
+                                    expr.Item1, expr.Item3));
+            }
+        }
+
+        private void ApplyOptToDescendents(int index, Expr expr, string tempVar)
+        {
+            var bs = controlFlowGraph.blocks;
+
+            var outputIndexes = controlFlowGraph.cfg.GetOutputNodes(index);
+            foreach (var ii in outputIndexes)
+            {
+                var ancestor_it = bs[ii].First;
+                while (ancestor_it.Value.result != expr.Item1.ToString()
+                        && ancestor_it.Value.result != expr.Item3.ToString())
+                {
+                    if (ExprEq((ancestor_it.Value.arg1,
+                                ancestor_it.Value.operation,
+                                ancestor_it.Value.arg2), expr))
+                        ancestor_it.Value = new ThreeCode(ancestor_it.Value.result,
+                            new ThreeAddressStringValue(tempVar));
+                    if (ancestor_it == bs[ii].Last)
+                        break;
+                    ancestor_it = ancestor_it.Next;
+                }
+                if (Outs[ii].Contains(expr))
+                    ApplyOptToDescendents(ii, expr, tempVar);
+            }
+        }
+
+        private bool ExprEq(Expr left, Expr right)
+        {
+            return left.Item1?.ToString() == right.Item1?.ToString()
+                && left.Item2 == right.Item2
+                && left.Item3?.ToString() == right.Item3?.ToString();
+        }
+
+        private string GenTempVariable()
+        {
+            string res = "#t" + currentTempVarIndex.ToString();
+            currentTempVarIndex++;
+            return res;
+        }
+    }
+}
